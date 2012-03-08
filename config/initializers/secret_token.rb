0e9e7fbff201ecee88dec28e429cd84dd3b0e3dd
Stefan Lindbohm <stefan@kolizz.com>
2012-03-08 17:33:01 +0100

    Init app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartdj::Application.config.secret_token = '1f56fcbacd94a1f1fcc517364465a8c2dbb50db6972d78b4e32f0573c858e7284da0dc215616c0a1ae96457ae8a1b9ff8a5517c6bdd048e5113c3091a415cca0'
